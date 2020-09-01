29a050108ec035f72fba6c1282b931d2fb159708
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-08-31 21:36:52 -0500

    sample Catch2 example

--- src/examples/01_module/02_int/vars.h
+++ src/examples/01_module/02_int/vars.h
@@ -1,1 +1,2 @@
 //Create prototype for void function echo_variable with one int parameter
+int sum_numbers(int num1, int num2);
--- test/examples_test/01_module_test/01_module_tests.cpp
+++ test/examples_test/01_module_test/01_module_tests.cpp
@@ -1,10 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "numbers.h"
+#include "vars.h"
 #include<string>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test sum_numbers function")
+{
+	REQUIRE(sum_numbers(5, 5)  == 10);
 }
 
 /*test case echo_variable */
