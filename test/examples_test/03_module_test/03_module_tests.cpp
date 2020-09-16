b51ac2b71ee29adda212b21e29739f9de435bbb1
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-09-16 18:35:39 -0500

    switch and immediate if examples

--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "if.h"
 #include "if_else.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -28,4 +29,14 @@ TEST_CASE("Test get generation function")
 	REQUIRE(get_generation(1950) == "Baby boomer");
 	REQUIRE(get_generation(1940) == "Silent Generation");
 	REQUIRE(get_generation(1776) == "Invalid Year");
+}
+
+TEST_CASE("Test switch menu function")
+{
+	REQUIRE(menu(0) == "Invalid Option");
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(5) == "Invalid Option");
 }
