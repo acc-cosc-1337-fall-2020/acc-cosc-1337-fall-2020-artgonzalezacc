ab7c95209bf5092e33dc8cd79dfeaeacad2bf5c7
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-09-17 10:01:25 -0500

    while loop example

--- src/examples/04_module/01_while/main.cpp
+++ src/examples/04_module/01_while/main.cpp
@@ -1,7 +1,22 @@
 #include "while.h"
+#include<iostream>
+
+using std::cout;	using std::cin;
 
 int main() 
 {
+	int value = 1;
+
+	if( !(value < 1) || value > 1 )cout<<"true";
+
+	char choice = 'y';
+
+	while(choice == 'y' || choice == 'Y')
+	{
+
+		cout<<"Enter y to continue, any other char to exit.";
+		cin>>choice;
+	}
 
 	return 0;
 }
