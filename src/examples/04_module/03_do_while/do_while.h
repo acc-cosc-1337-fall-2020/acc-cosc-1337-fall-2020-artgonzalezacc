2cdcd73d03dca87214eb81c7a187f6ed1c1252fd
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-09-21 19:01:50 -0500

    do while example

--- src/examples/04_module/03_do_while/do_while.h
+++ src/examples/04_module/03_do_while/do_while.h
@@ -1,6 +1,11 @@
 //Write void function protype named prompt_user with no parameters
+void prompt_user();
 
+void display_menu();
+
+void execute_menu(int option);
 
 
 //Write void function protype named run_menu with no parameters
+
 
