a8ed7051f11caa307f33ad9d55b01c6af9ed8ed4
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-11-06 19:49:14 -0600

    hw7 tictactoe3 main

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,15 +1,18 @@
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_manager.h"
 
 using std::cin; using std::cout;
 
 int main() 
 {
-	TicTacToe tic_tac_toe;
+	TicTacToeManager manager;
 	std::string player;
 	char cont;
 
 	do
 	{
+		TicTacToe tic_tac_toe;
+
 		while(player != "X" && player != "O")
 		{
 			std::cout<<"Enter X or O: ";
@@ -27,11 +30,14 @@ int main()
 
 		std::cout<<"Game over";
 		player = "";
+		manager.save_game(tic_tac_toe);
 		
 		cout<<"Play again? y or Y ";
 		cin>>cont;
 
 	}while(cont == 'y' || cont == 'Y');
+
+	cout<<manager;
 
 	return 0;
 }
