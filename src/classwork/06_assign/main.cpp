a36cb0e9976833be33975bfbce49fce7f81e7384
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-11-11 18:14:01 -0600

    File input ouput text

--- src/classwork/06_assign/main.cpp
+++ src/classwork/06_assign/main.cpp
@@ -1,17 +1,45 @@
-//write include statemetns
+#include<fstream>
+#include<iostream>
+#include<string>
 
-/*
-Create a vector of rectangles
-Add 3 Rectangle classes to the vector:
-Width	Height		Area
-  4		 5			  20
- 10		10			 100
-100		10			1000
-Iterate the vector and display the Area for each Rectangle on one line and the total area for the
-3 rectangles.
-*/
+using std::cout;
+
 int main()
 {
+  std::ofstream out_file;
+
+  cout<<"Open file\n";
+  out_file.open("names.dat");
+
+  cout<<"Writing names to file\n";
+  out_file<<"joe\n";
+  out_file<<"mary\n";
+  out_file<<"john\n";
+
+  cout<<"Done\n\n";
+
+  out_file.close();
+
+  std::ifstream in_file;
+  cout<<"Open file for reading\n";
+  in_file.open("names.dat");
+
+  std::string name;
+
+  if(in_file.is_open())
+  {
+    cout<<"File opened-- try read \n";
+    while(in_file>>name)
+    {
+      cout<<"Read value: "<<name<<"\n";
+    }
+  }
+
+  cout<<"Close file\n";
+
+  in_file.close();
 	
+  cout<<"complete";
+
 	return 0;
 }
