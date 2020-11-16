d883cdd1c4e8ab9b9ba7fc7cbc36b2b19a267663
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-11-16 17:49:12 -0600

    file in and out with temperature objects

--- src/examples/08_module/main.cpp
+++ src/examples/08_module/main.cpp
@@ -1,6 +1,26 @@
+#include "temperature_data.h"
+#include<iostream>
 
 int main() 
 {
+	std::vector<Temperature> out_temps
+	{
+		Temperature(1, 98.9), Temperature(2, 98.5), Temperature(3, 99.1),
+		Temperature(4, 99.5), Temperature(5, 99.0), Temperature(6, 99.9)
+	};
+
+	TemperatureData data;
+	std::cout<<"Save vector to file\n";
+	data.save_temps(out_temps);
+
+	std::cout<<"Read file and recreate vector \n";
+
+	std::vector<Temperature> in_temps = data.get_temps();
+
+	for(auto temp: in_temps)
+	{
+		std::cout<<temp.get_hour()<<" "<<temp.get_reading()<<"\n";
+	}
 
 	return 0;
 }
--- src/examples/08_module/temperature_data.h
+++ src/examples/08_module/temperature_data.h
@@ -1,1 +1,19 @@
 //temperature_data.h
+#include<fstream>
+#include<string>
+#include<vector>
+#include "temperature.h"
+
+#ifndef TEMPERATURE_DATA_H
+#define  TEMPERATURE_DATA_H
+
+class TemperatureData
+{
+public:
+    void save_temps(std::vector<Temperature>& temps);
+    std::vector<Temperature> get_temps();
+private:
+    const std::string file_name {"temperature.dat"};
+};
+
+#endif
