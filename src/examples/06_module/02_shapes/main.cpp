a4967ec4beab95c32c52e907068a59ea43bbf84a
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-11-19 23:31:07 -0600

    dynamic array of objects

--- src/examples/06_module/02_shapes/main.cpp
+++ src/examples/06_module/02_shapes/main.cpp
@@ -15,7 +15,31 @@ using std::unique_ptr; using std::make_unique;
 
 int main() 
 {	
-	unique_ptr<Shape> circle = make_unique<Circle>();
+	Shape* ptr = new Circle[3];
+	ptr[0].draw();
+	
+	std::cout<<"pointer to pointer dynamic array\n";
+	Shape** shapes1 = new Shape*[3];
+	shapes1[0] = new Circle();
+	shapes1[1] = new Line();
+	shapes1[2] = new Circle();
+
+	shapes1[0]->draw();
+	shapes1[1]->draw();
+	shapes1[2]->draw();
+	std::cout<<"\n ENDpointer to pointer dynamic array\n";
+
+	//use ptr
+
+	Circle circles[3];
+	circles[0].draw();
+	circles[1].draw();
+	circles[2].draw();
+
+	Circle circle = circles[0];
+	circle.draw();
+
+/*	unique_ptr<Shape> circle = make_unique<Circle>();
 	circle->draw();
 
 	unique_ptr<Shape> line = make_unique<Line>();
@@ -29,7 +53,7 @@ int main()
 	for(auto& shape: shapes)
 	{
 		shape->draw();
-	}
+	}*/
 
 	return 0;
 }
