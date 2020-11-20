2ede2803128aeb00d53e70d69d9874ff1d637036
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-11-19 23:30:38 -0600

    pointers, arrays, and array parameters

--- src/examples/10_module/05_arrays_functions/main.cpp
+++ src/examples/10_module/05_arrays_functions/main.cpp
@@ -1,8 +1,17 @@
 #include "arr_functions.h"
 
+using std::cout;
+
 int main() 
 {
-	
+	int times_table[ROWS][COLS];
+
+	populate_times_table(times_table, ROWS);
+
+	display_times_table(times_table, ROWS);
+
+	int nums[ROWS]{};
+	display_array(nums, ROWS);
 
 	return 0;
 }
